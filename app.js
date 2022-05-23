c0859223dea340c0311e01c89c130d4268cc8cd9
Loick Hommet <hommet.loick@gmail.com>
2022-05-23 13:48:34 +0200

    Route Get api/tache

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const app = express();
 
 app.use(express.json());
 
+const mapToObj = (m) => {
+    return Array.from(m).reduce((obj, [key, value]) => {
+      obj[key] = value;
+      return obj;
+    }, {});
+  };
+
+  app.get('/api/tache', (req, res) => {
+    res.json(mapToObj(db.memoryDb));
+})
+
+
 
 
 
