588a35c9c73a8e92a2512edbf5a5934648ef82af
Loick Hommet <hommet.loick@gmail.com>
2022-05-23 15:27:26 +0200

    Créer fonction register

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const db = require('./db');
+const Collection = require('./Collection');
 const Joi = require("joi");
 // const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+const Accounts = new Collection('Accounts');
 
 const app = express();
 
@@ -16,6 +17,8 @@ const hash = (mdp, salt) =>{
 //console.log(hash("test", 123))  
 
 app.use(express.json());
+
+// TACHE
 
 const mapToObj = (m) => {
     return Array.from(m).reduce((obj, [key, value]) => {
@@ -78,7 +81,37 @@ app.get('/api/tache/:id', (req, res) => {
   return res.status({ message: 'Deleted' }).end();
   })
 
+  // USER
 
+  app.post('/register', async (req, res) => {
+    const payload = req.body;
+
+    const scheme = Joi.object({
+        email: Joi.string().max(255).email().required(),
+        password: Joi.string().max(255).required(),
+        username: Joi.string().max(255).required()
+    });
+    const {value: account, error} = scheme.validate(payload);
+    if (error) {
+        throw new Error(error.details[0].message);
+    }
+
+    const {id, found} = Accounts.findByProperty('email', account.email);
+    if (found) {
+        throw new Error("Un compte existe déja");
+    }
+    // Hash du mot de passe avec bcrypt
+    // const salt = await bcrypt.genSalt(10);
+    // const passwordHash = await bcrypt.hash(account.password, salt);
+   
+    const salt = 1245;
+    const passwordHash = hash(account.password, salt)
+     account.password = passwordHash;
+
+    Accounts.insertOne(account);
+
+    res.status(201).send('L \' utilisateur est inscrit : ' + account.email);
+});
 
 
 module.exports = app;
